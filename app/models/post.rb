984806337cf49a6cc89ecf782663b24c46f8422c
eiichi-mamada <eastrage@gmail.com>
2020-02-06 16:44:29 +0900

    検索機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   validates :image, presence: true
   belongs_to :user
   belongs_to :area
-
   mount_uploader :image, ImageUploader
+
+  def self.search(search)
+    Post.where("area_id = #{search}")
+  end
 end
