b544e2571aa7677576173cedb1af47ae3d241999
eiichi-mamada <eastrage@gmail.com>
2020-02-06 19:15:21 +0900

    コメント機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   validates :image, presence: true
   belongs_to :user
   belongs_to :area
+  has_many :comments
   mount_uploader :image, ImageUploader
 
   def self.search(search)
